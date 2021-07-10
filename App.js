22419ec40ee3187135d25ed6cb4472fb67b861a7
Burak <burak@netfront.com.au>
2021-07-10 13:06:24 +1000

    initial 5

--- App.js
+++ App.js
@@ -1,32 +1,78 @@
 import React, {useState} from 'react';
-import {API_URL, API_TOKEN} from '@env';
+import {API_URL, API_KEY} from '@env';
 
 import {
   View,
   Text,
   StyleSheet,
   SafeAreaView,
-  Platform,
-  StatusBar,
+  TextInput,
   Button,
+  Image,
 } from 'react-native';
 
 const App = () => {
-  const [userName, setUserName] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [movie, setMovie] = useState([]);
 
-  console.log(`baseUrl`, API_URL);
-
-  const handleOnPress = () => {
-    const users = ['Burak', 'Ozlem', 'Cinar', 'Seyhan'];
-    const random = Math.floor(Math.random() * 10);
-    setUserName(users[random]);
+  const handleOnChangeSearch = term => {
+    setSearchTerm(term);
+    if (term.length) {
+      const url = `${API_URL}/movie/${term}?api_key=${API_KEY}`;
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(`object`, data.poster_path);
+          const {title, success} = data;
+          if (success || !title) {
+            setMovie('Movie not found');
+          }
+          if (title) {
+            setMovie(data);
+          }
+        })
+        .catch(function (error) {
+          console.log(
+            'There has been a problem with your fetch operation: ' +
+              error.message,
+          );
+        });
+    } else {
+      setMovie('Find your movie...');
+    }
   };
 
+  // const handleSearch = () => {
+  //   console.log(`searchTerm`, searchTerm);
+  //   const url = `${API_URL}search/movie/?api_key=${API_KEY}&query=jaws`;
+  //   console.log(`url`, url);
+  //   fetch(`${API_URL}search/movie/?api_key=${API_KEY}&query=jaws`)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log(data);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(
+  //         'There has been a problem with your fetch operation: ' +
+  //           error.message,
+  //       );
+  //     });
+  // };
+
   return (
-    <SafeAreaView style={styles.body}>
-      <View style={styles.container}>
-        <Text>Welcome to BODB {userName || 'Anonymous'}</Text>
-        <Button title="Set User" onPress={handleOnPress} />
+    <SafeAreaView>
+      <View style={styles.body}>
+        <View style={styles.container}>
+          <Text style={styles.header}>{movie.title}</Text>
+          {/* <Image source={{uri: `${movie.poster_path}`}} /> */}
+          <TextInput
+            style={styles.input}
+            onChangeText={handleOnChangeSearch}
+            value={searchTerm}
+            placeholder="Search movie"
+          />
+          {/* <Button title="Search Movie" onPress={handleSearch} /> */}
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -34,15 +80,21 @@ const App = () => {
 
 const styles = StyleSheet.create({
   body: {
-    flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
+    padding: 10,
   },
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'ios' ? StatusBar.currentHeight : 0,
+    margin: 20,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: '700',
+  },
+  input: {
+    marginTop: 10,
+    height: 40,
+    padding: 6,
+    borderWidth: 1,
   },
 });
 
